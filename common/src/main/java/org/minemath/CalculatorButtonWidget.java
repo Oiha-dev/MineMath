9b9214a97cf46b8822b486b4eec8126cb1cd34c5
oiha <cenes.gauthier@gmail.com>
2025-03-11 20:13:32 +0100

    feat(CalculatorButtonWidget): add hover and click textures for button interactions

--- common/src/main/java/org/minemath/CalculatorButtonWidget.java
+++ common/src/main/java/org/minemath/CalculatorButtonWidget.java
@@ -8,17 +8,40 @@ import net.minecraft.util.Identifier;
 public class CalculatorButtonWidget extends ButtonWidget {
 
     private final Identifier texture = new Identifier("minemath", "textures/buttons/buttons-spritesheet.png");
+    private final Identifier textureOnHover = new Identifier("minemath", "textures/buttons/buttons-spritesheet-hover.png");
+    private final Identifier textureOnClick = new Identifier("minemath", "textures/buttons/buttons-spritesheet-clicked.png");
+
     private final int spriteNbr;
+    private boolean isClicked = false;
 
     public CalculatorButtonWidget(int x, int y, int id) {
         super(x, y, 16, 13, Text.empty(), button -> {
             System.out.println(id);
+
         }, ButtonWidget.DEFAULT_NARRATION_SUPPLIER);
         this.spriteNbr = id;
     }
 
     @Override
     protected void renderButton(DrawContext context, int mouseX, int mouseY, float delta) {
-        context.drawTexture(texture, this.getX(), this.getY(), spriteNbr * 16, 0, 16, 13, 560, 13);
+        if (isClicked) {
+            context.drawTexture(textureOnClick, this.getX(), this.getY(), spriteNbr * 16, 0, 16, 13, 560, 13);
+        }else if (this.isHovered()) {
+            context.drawTexture(textureOnHover, this.getX(), this.getY(), spriteNbr * 16, 0, 16, 13, 560, 13);
+        } else {
+            context.drawTexture(texture, this.getX(), this.getY(), spriteNbr * 16, 0, 16, 13, 560, 13);
+        }
+    }
+
+    @Override
+    public void onClick(double mouseX, double mouseY) {
+        super.onClick(mouseX, mouseY);
+        isClicked = true;
+    }
+
+    @Override
+    public void onRelease(double mouseX, double mouseY) {
+        super.onRelease(mouseX, mouseY);
+        isClicked = false;
     }
 }
