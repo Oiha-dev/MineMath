10e6db7d4e40ca74700de52b192953799cf8c93f
oiha <cenes.gauthier@gmail.com>
2025-03-11 12:49:06 +0100

    feat(CustomScreen): add result text field and update calculator placement

--- common/src/main/java/org/minemath/CustomScreen.java
+++ common/src/main/java/org/minemath/CustomScreen.java
@@ -3,29 +3,40 @@ package org.minemath;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.gui.widget.TextWidget;
+import net.minecraft.client.util.Icons;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import java.awt.*;
 
 
 public class CustomScreen extends Screen {
 
+    protected TextFieldWidget resultWidget;
     final static Identifier calculatorTexture = new Identifier("minemath", "textures/calculator/calculator.png");
-
     public CustomScreen(Text title) {
         super(title);
     }
 
     @Override
     protected void init() {
+        int weightScale = MinecraftClient.getInstance().getWindow().getScaledWidth();
+        int heightScale = MinecraftClient.getInstance().getWindow().getScaledHeight();
 
+        this.resultWidget = new TextFieldWidget(this.textRenderer, weightScale/2-42, heightScale/2-59, 77, 20, Text.of("grdg"));
+        this.resultWidget.setDrawsBackground(false);
+        addDrawableChild(this.resultWidget);
     }
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        super.render(context, mouseX, mouseY, delta);
+        int weightScale = MinecraftClient.getInstance().getWindow().getScaledWidth();
+        int heightScale = MinecraftClient.getInstance().getWindow().getScaledHeight();
 
-        context.drawTexture(calculatorTexture, 0, 0, 0, 0, 110, 154, 110, 154);
-        context.drawText(this.textRenderer, "Special Button", 40, 40 - this.textRenderer.fontHeight - 10, 0xFFFFFFFF, true);
+        context.drawTexture(calculatorTexture, weightScale/2-55, heightScale/2-77, 0, 0, 110, 154, 110, 154);
+
+        super.render(context, mouseX, mouseY, delta);
     }
 }
